72fe81002423ee7ab578459135b63402f43aa515
tagabenz <tagabenz@gmail.com>
2022-08-30 15:12:44 +0300

    add js1

--- stream_app/static/jss/add_contents.js
+++ stream_app/static/jss/add_contents.js
@@ -1,14 +1,4 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
-
-if (hourNow>18){
-    greeting='Добрый вечер!';
-}else if(hourNow>12){
-    greeting='Добрый день!';
-}else if(hourNow>0){
-    greeting='Доброе утро!';
-}else{
-    greeting='Приветствуем!';
+var hotItems=document.querySelectorAll('li.hot');
+for(var i=0;i<hotItems.length;i++){
+  hotItems[i].className='cool';
 }
-document.write('<h3>'+greeting+'</h3>');   
