0419deed0703e0a7513db62557b520a37e599a0f
tagabenz <tagabenz@gmail.com>
2022-09-01 15:19:09 +0300

    edit Header block

--- stream_app/static/jss/add_contents.js
+++ stream_app/static/jss/add_contents.js
@@ -1,4 +1,13 @@
-var hotItems=document.querySelectorAll('li.hot');
-for(var i=0;i<hotItems.length;i++){
-  hotItems[i].className='cool';
+var listItems=document.querySelectorAll('li');
+
+var i;
+for(i=0;i<listItems.length;i++){
+  listItems[i].className='cool';
 }
+
+var heading=document.querySelector('h2');
+var headingText=heading.firstChild.nodeValue;
+
+var totalItems=listItems.length;
+var newHeading = headingText+ '<span>' +totalItems+ '</span>';
+heading.innerHTML=newHeading;
