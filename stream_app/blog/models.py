589a67af790f37e2a6acc5e0dd71b2d96b7bcf94
tagabenz <tagabenz@gmail.com>
2023-01-09 13:55:58 +0300

    add Blog app

--- stream_app/blog/models.py
+++ stream_app/blog/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class Blogs(models.Model):
     title=models.CharField(max_length=1000, verbose_name='Заголовок')
-    img_url=models.CharField(max_length=1000)
-    # description = models.TextField(blank=True)
+    img_url=models.CharField(max_length=1000,default='')
+    description = models.CharField(max_length=1000,default='',verbose_name='Описание')
 
     def __str__(self):
         return str(f"id:{self.id} {self.title} ")
