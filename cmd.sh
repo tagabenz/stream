17c296f5ea538727722261e34c708ed00e0db9cd
tagabenz <tagabenz@gmail.com>
2023-01-20 05:19:35 +0300

    1231231231

--- cmd.sh
+++ cmd.sh
@@ -4,8 +4,8 @@ set -e
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
   #exec python manage.py runserver 0.0.0.0:8000
-  #exec la -la
-  exec pwd
+  exec la -la
+  #exec pwd
 elif [ "$ENV" = 'STAGE' ]; then
   echo "Running Staging Server"
 
