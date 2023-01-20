130d1d3703aa4b79fd5e07cd6cff9a119d016b85
tagabenz <tagabenz@gmail.com>
2023-01-20 04:33:06 +0300

    add jenkins.yml for stage

--- scripts/triger.sh
+++ scripts/triger.sh
@@ -1,2 +1,2 @@
-#! /bin/sh
-curl http://172.23.226.22:8080/job/stream_develop/build?token=1q2w3e4r
+#!/bin/sh
+curl -I -u admin:1234 "http://192.168.1.168:8080/job/stream_develop/build?token=1q2w3e4r"
