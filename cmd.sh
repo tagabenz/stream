eb884ab249aba888b6ed5c9394abb2ffc4ab4207
tagabenz <tagabenz@gmail.com>
2023-01-27 13:43:46 +0300

    edit cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,8 +3,7 @@ set -e
 
 if [ "$ENV" = 'DEV' ]; then
   echo "Running Development Server"
-  exec python stream_app/manage.py runserver 0.0.0.0:8000
-
+  exec python ../manage.py runserver 0.0.0.0:8000
 elif [ "$ENV" = 'STAGE' ]; then
   echo "Running Staging Server"
   gunicorn stream_app.wsgi:application --bind 0.0.0.0:8000
--- stream_app/static/jss/content.js
+++ stream_app/static/jss/content.js
@@ -1,1 +1,8 @@
+window.onload=function () {
+  document.onclick=hideDetails;
+}
 
+function hideDetails() {
+  var details = document.querySelector('details');
+  details.open = false;
+}
