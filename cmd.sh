c38acd094f36fc9d9fb435436ba324423d40ed65
tagabenz <tagabenz@gmail.com>
2023-01-31 13:19:59 +0300

    add prod.yml

--- cmd.sh
+++ cmd.sh
@@ -7,4 +7,7 @@ if [ "$ENV" = 'DEV' ]; then
 elif [ "$ENV" = 'STAGE' ]; then
   echo "Running Staging Server"
   gunicorn stream_app.wsgi:application --bind 0.0.0.0:8000
+elif [ "$ENV" = 'PROD' ]; then
+  echo "Running Production Server"
+  gunicorn stream_app.wsgi:application --bind 0.0.0.0:8000  
 fi
