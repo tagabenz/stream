f2981f27c8a0bc914dee948af734c2d978fcf184
tagabenz <tagabenz@gmail.com>
2023-02-04 14:50:27 +0300

    edit triger.sh

--- scripts/triger.sh
+++ scripts/triger.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -I -u admin:1234 "http://192.168.1.168:8080/job/stream_develop/build?token=1q2w3e4r"
+curl -I -u admin:1234 "http://192.168.1.198:8080/job/stream_develop/build?token=1q2w3e4r"
