7fbed54d8b9889269e766e8eb96d83d172d3bfe9
tagabenz <tagabenz@gmail.com>
2023-03-30 14:06:35 +0300

    add inclusion tags

--- stream_app/homepage/templatetags/homepage_tags.py
+++ stream_app/homepage/templatetags/homepage_tags.py
@@ -3,7 +3,16 @@ from homepage.models import *
 
 register=template.Library()
 
-@register.simple_tag()
-def get_menu():
-    return Menu.objects.all()
+# @register.simple_tag()
+# def get_menu():
+#     return Menu.objects.all()
 
+
+@register.inclusion_tag('show_menu.html')
+def show_menu(sort=None,cat_selected=0):
+    if not sort:
+        menu=Menu.objects.all()
+    else:
+        menu=Menu.objects.order_by(sort)    
+    
+    return {"menu":menu, "cat_selected":cat_selected,}
