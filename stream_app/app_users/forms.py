fee2d91c6710d269e71a5e84a712b706f3d9e23d
tagabenz <tagabenz@gmail.com>
2023-04-05 17:33:58 +0300

    edit login.html

--- stream_app/app_users/forms.py
+++ stream_app/app_users/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class AuthForm(forms.Form):
-    username=forms.CharField(label="Логин")
-    password=forms.CharField(label="Пароль")
+    username=forms.CharField(min_length='3', max_length='20', label="Логин", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "Логин",}))
+    password=forms.CharField(min_length='8', max_length='20',label="Пароль", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': "Пароль",}))
