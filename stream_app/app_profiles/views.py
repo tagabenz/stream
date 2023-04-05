7dd4142c826c5621d054e0b4a45391f7c3507ba4
tagabenz <tagabenz@gmail.com>
2023-04-06 00:18:17 +0300

    add title in reg

--- stream_app/app_profiles/views.py
+++ stream_app/app_profiles/views.py
@@ -7,16 +7,26 @@ from app_profiles.models import User
 
 
 class UserFormView(View):
-
+    
+    
     def get(self, request):
         user_form=UserForm()
-        return render(request, 'registration.html', context={'user_form': user_form})
+        context={
+            'user_form': user_form,
+            'title' : "Регистрация - Lastream.online",
+        }
+
+        return render(request, 'registration.html', context=context)
     
     def post(self, request):
         user_form=UserForm(request.POST)
-
+        context={
+            'user_form': user_form,
+            'title' : "Регистрация - Lastream.online",
+        }
         if user_form.is_valid():
             User.objects.create(**user_form.cleaned_data)
             return HttpResponseRedirect('/')
+        
         return render(request, 'registration.html', context={'user_form': user_form})    
 
