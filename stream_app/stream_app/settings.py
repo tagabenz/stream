5b51fb9bd11e0ed758cedb73969f8ef72ac95deb
tagabenz <tagabenz@gmail.com>
2023-05-09 08:25:46 +0300

    some edit

--- stream_app/stream_app/settings.py
+++ stream_app/stream_app/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+print(os.getenv('HOST'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
